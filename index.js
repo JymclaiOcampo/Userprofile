0505a792af4ff329cb58ca129a2549853a29eedd
jinwu09 <61149832+jinwu09@users.noreply.github.com>
2022-12-01 22:12:46 +0800

    task done design made

--- index.js
+++ index.js
@@ -3,14 +3,32 @@ const img = document.querySelector("#photo");
 const file = document.querySelector("#file");
 const uploadebtn = document.querySelector("#uploadbtn");
 
-file.addEventListener("change", function () {
-  const chosedfile = this.files[0];
-  if (chosedfile) {
+function importData() {
+  let input = document.createElement("input");
+  input.type = "file";
+  input.onchange = (_) => {
+    // you can use this method to get file and perform respective operations
+    let files = Array.from(input.files);
+    // console.log(files);
+    const file = files;
+    // console.log(file[0]);
     const reader = new FileReader();
 
     reader.addEventListener("load", function () {
       img.setAttribute("src", reader.result);
     });
-    reader.readAsDataURL(chosedfile);
-  }
-});
+    reader.readAsDataURL(file[0]);
+  };
+  input.click();
+}
+// file.addEventListener("change", function () {
+//   const chosedfile = this.files[0];
+//   if (chosedfile) {
+//     const reader = new FileReader();
+
+//     reader.addEventListener("load", function () {
+//       img.setAttribute("src", reader.result);
+//     });
+//     reader.readAsDataURL(chosedfile);
+//   }
+// });
